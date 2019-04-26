0f67260d31ecb1d1ad2a9581c34563c75174eaf4
andreas-fischnaller <andreas@nordavinden.com>
2019-04-27 00:15:51 +0200

    Update style

--- js/main.js
+++ js/main.js
@@ -62,12 +62,12 @@ function createUI()
         {
             check = "";
         }
-        if (dictionaries[lang][i].directInput == false) style = `style="border:1px solid rgb(255, 255, 255); background-color: rgba(255, 0, 0, 0.25)"`;
-        else style = `style="border:1px solid rgb(255, 255, 255); background-color: rgba(0, 255, 0, 0.25)"`;
-        document.getElementById("dictionary_select").innerHTML += `
-        <span id="dictionary_label" ${style}><label>
-            <input type="radio" name="dictionary" value="${dictionaries[lang][i].value}" onclick="dictionary = this.value;"${check}/> ${dictionaries[lang][i].name} 
-        </label></span> `;
+        if (dictionaries[lang][i].directInput == false) style = `style="background-color: rgba(255, 0, 0, 0.25)"`;
+        else style = `style="background-color: rgba(0, 255, 0, 0.25)"`;
+        document.getElementById("dictionary_select").innerHTML += `        
+            <label ${style}>
+                <input type="radio" name="dictionary" value="${dictionaries[lang][i].value}" onclick="dictionary = this.value;"${check}/> ${dictionaries[lang][i].name} 
+            </label> `;
     }                    
 }
 
